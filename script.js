f25b2a38b1042e021d1d69898ad3fa4fd6c4adc3
vcorneau <vcorneau@gmail.com>
2023-06-16 10:22:22 -0400

    Add DOM elements containing info from myLibrary array

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
-let myLibrary = ["buns", "butts", "bingo"];
-
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
-  if (read === true) {
+  if (read) {
     this.read = "has been read"
   } else {
     this.read = "has not been read"
   }
-  this.info = function() {
-    return title + " by " + author + ", " + pages + " pages, " + this.read
-  }
 }
 
-/*Write a function that loops through the array and displays 
-each book on the page. You can display them in some sort of table, 
-or each on their own “card”. It might help for now to manually add a 
-few books to your array so you can see the display.*/
+//Temporary code adding books to the array
+let bookTemp1 = new Book("You Better Be Lightning", "Andrea Gibson", 115, false);
+let bookTemp2 = new Book("The Midnight Library", "Matt Haig", 304, false);
+let bookTemp3 = new Book("The Martian", "Andrew Weir", 384, true);
+let myLibrary = [bookTemp1, bookTemp2, bookTemp3];
+//End of temporary code
+
+console.log(myLibrary);
 
 function displayBooks() {
   let i = 0;
-  while (i <= myLibrary.length-1){
-    console.log(myLibrary.at(i));
+  while (i < myLibrary.length){
+    let newCard = document.createElement("div")
+    newCard.className = "bookCard";
+
+    let title = document.createTextNode(myLibrary.at(i).title);
+    let author = document.createTextNode(myLibrary.at(i).author);
+    let pages = document.createTextNode(myLibrary.at(i).pages);
+    let read = document.createTextNode(myLibrary.at(i).read);
+
+    newCard.appendChild(title);
+    newCard.appendChild(author);
+    newCard.appendChild(pages);
+    newCard.appendChild(read);
+
+    const element = document.getElementById("inventory");
+    element.appendChild(newCard);
     i++
     }
   }
+
+  displayBooks();
